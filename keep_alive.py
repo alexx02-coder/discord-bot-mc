008fdcde442ed2d21a038d31d08b6ef7c1f906f0
alexx02-coder <alexisdubesset@gmail.com>
2026-03-20 21:43:37 +0100

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import os
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Discord en ligne !"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
